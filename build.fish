59da0b2b74ee5ddfbffc703635579a05dab60287
xoryves <xoryves@googlemail.com>
2023-05-27 11:59:26 +0200

    Add build.fish

---
+++ build.fish
@@ -0,0 +1,32 @@
+#!/usr/bin/env fish
+
+set BRANCH 'site'
+set REV (git rev-parse HEAD)
+set REV_LOG (git log -1 --pretty=format:%s $BRANCH)
+if test "$REV" = "$REV_LOG"
+  echo "already deployed"
+  exit 0
+end
+
+git stash save
+if not npm run build
+  echo 'npm run build failed!'
+  exit 1
+end
+if not bundle exec nanoc
+  echo 'bundle exec nanoc failed!'
+  exit 1
+end
+if not git checkout $BRANCH
+  echo "missing orphan branch '$BRANCH'"
+  exit 1
+end
+git rm -rf .
+git checkout master -- .gitignore
+mv output/* . && rm -rf output
+git add .
+git commit -m "$REV"
+git push xoryves $BRANCH
+
+git checkout master
+git stash pop
